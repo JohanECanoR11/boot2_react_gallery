146ab624b310ade732df62b3b8133444c567e5e8
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-07-24 11:29:16 -0500

    Routes configuration with react-router-dom for SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
-import { Orquidea1 } from './components/Orquidea1'
-import { Orquidea2 } from './components/Orquidea2'
-import { Orquidea3 } from './components/Orquidea3'
-import { Orquidea4 } from './components/Orquidea4'
-import { Orquidea5 } from './components/Orquidea5'
-import { Orquidea6 } from './components/Orquidea6'
+import { Orquidea1 } from './components/Orquidea1';
+import { Orquidea2 } from './components/Orquidea2';
+import { Orquidea3 } from './components/Orquidea3';
+import { Orquidea4 } from './components/Orquidea4';
+import { Orquidea5 } from './components/Orquidea5';
+import { Orquidea6 } from './components/Orquidea6';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
@@ -12,12 +13,16 @@ function App() {
 
   return (
     <>
-      <Orquidea1 />
-      <Orquidea2 />
-      <Orquidea3 />
-      <Orquidea4 />
-      <Orquidea5 />
-      <Orquidea6 />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/orquidea1' element={<Orquidea1 />} />
+          <Route path='/orquidea2' element={<Orquidea2 />} />
+          <Route path='/orquidea3' element={<Orquidea3 />} />
+          <Route path='/orquidea4' element={<Orquidea4 />} />
+          <Route path='/orquidea5' element={<Orquidea5 />} />
+          <Route path='/orquidea6' element={<Orquidea6 />} />
+        </Routes>  
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Orquidea1.jsx
+++ src/components/Orquidea1.jsx
@@ -2,6 +2,8 @@ import orquidea1 from '../assets/img/orquidea1.png'
 
 export const Orquidea1 = () => {
   return (
-    <img src={orquidea1} alt="Orquídea Calanthe" />
+    <div>
+      <img src={orquidea1} alt="Orquídea Calanthe" />
+    </div>    
   )
 }
